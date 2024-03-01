8bb89f16cd15cf4f8510a52f51165f604ff62ce8
Vard69 <luckaslervik@hotmail.com>
2024-03-01 10:45:00 +0100

    better graphics

--- main.js
+++ main.js
@@ -8,11 +8,10 @@ context.clearRect(0, 0, ctx.width, ctx.height)
 
 var myChart = new Chart(context, {
     type: 'line',
-
     data: {
-        labels: ["6 months ago","5 months ago","4 months ago","3 months ago","2 months ago","1 month ago","now"],
+        labels: ["½ a Year ago","5 months ago","4 months ago","3 months ago","2 months ago","1 month ago","now"],
         datasets: [{
-            label: "Money",
+            label: "",
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
             data: [jack, jon, jen, jill, jane, john, james]
@@ -127,7 +126,7 @@ function obama() {
   jane = john
   john = james
   james = kennethblevinsjohnsimpsonaberothdonbirdrudolphrogers
-  document.getElementById("clark").innerHTML = "Money: " + kennethblevinsjohnsimpsonaberothdonbirdrudolphrogers;
+  document.getElementById("clark").innerHTML = "💲: " + kennethblevinsjohnsimpsonaberothdonbirdrudolphrogers;
   edward()
 
 
